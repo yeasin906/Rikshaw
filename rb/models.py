2f1ba71dcf283faeaf14bbda69ce53258690bac9
Yeasin Chowdhury <easinmahi630@gmail.com>
2024-04-23 11:36:34 +0600

    6th commit

--- rb/models.py
+++ rb/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Rider(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=15)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
