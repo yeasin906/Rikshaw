02a7df594b6693dc6e2a7edeb30f062922c17e80
NusratNaff <21201050@uap-bd.edu>
2024-05-02 00:54:44 +0600

    Fifth Commit

--- rb/views.py
+++ rb/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login')
 def home(request):
     return render(request,'home.html')
 def index(request):
